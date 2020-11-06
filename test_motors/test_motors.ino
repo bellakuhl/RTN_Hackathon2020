c5da6b2a58b419a5e90199148c2c29442ac3ad92
Isabella Kuhl <iamkuhl@bu.edu>
2020-11-06 17:05:09 -0500

    Pushed motor test script

---
+++ test_motors/test_motors.ino
@@ -0,0 +1,40 @@
+const int enAPin = 6; // Left motor PWM speed control
+const int in1Pin = 7; // Left motor Direction 1
+const int in2Pin = 5; // Left motor Direction 2
+const int in3Pin = 4; // Right motor Direction 1
+const int in4Pin = 2; // Right motor Direction 2
+const int enBPin = 3; // Right motor PWM speed control
+enum Motor { LEFT, RIGHT };
+
+void setup() {
+  pinMode(enAPin, OUTPUT);
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+  pinMode(in3Pin, OUTPUT);
+  pinMode(in4Pin, OUTPUT);
+  pinMode(enBPin, OUTPUT);
+
+}
+
+void loop() {
+  testMotors();
+}
+
+// Set motor speed: 255 full ahead, -255 full reverse , 0 stop
+void go( enum Motor m, int speed){
+  digitalWrite (m == LEFT ? in1Pin : in3Pin , speed > 0 ? HIGH : LOW );
+  digitalWrite (m == LEFT ? in2Pin : in4Pin , speed <= 0 ? HIGH : LOW );
+  analogWrite(m == LEFT ? enAPin : enBPin, speed < 0 ? -speed : speed );
+}
+
+// Initial motor test :
+// left motor forward then back
+// right motor forward then back
+void testMotors () {
+  static int speed[8] = { 128, 255, 128, 0 ,-128, -255, -128, 0};
+  go(RIGHT, 0);
+  for (unsigned char i = 0 ; i < 8 ; i++)
+    go(LEFT, speed[i ]), delay (200);
+  for (unsigned char i = 0 ; i < 8 ; i++)
+    go(RIGHT, speed[i ]), delay (200);
+}
